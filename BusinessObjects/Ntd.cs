7a7d0847ac8aa614f630269ade03f78748e9d358
Ntd93 <89728121+NTDung93@users.noreply.github.com>
2023-08-15 11:59:04 +0700

    Add Ntd class (#2)

---
+++ BusinessObjects/Ntd.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessObjects
+{
+    internal class Ntd
+    {
+        public string name { get; set; }
+    }
+}
