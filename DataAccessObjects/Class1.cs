1a36d98dc0368597cf4b38f6512b1ce547275b2d
satoshiuy <lequocuyit@gmail.com>
2023-08-15 07:10:24 +0700

    setup enviroment and folder structure

---
+++ DataAccessObjects/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace DataAccessObjects
+{
+    public class Class1
+    {
+    }
+}
---
+++ Repositories/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Repositories
+{
+    public class Class1
+    {
+    }
+}
