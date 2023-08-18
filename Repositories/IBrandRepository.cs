605214f111ba5eec452bcdd0e64a1e374f0487a3
Le Quoc Uy <91858657+UyLeQuoc@users.noreply.github.com>
2023-08-18 20:47:00 +0700

    add CRUD Model and CRUD Brand (#15)

---
+++ Repositories/IBrandRepository.cs
@@ -0,0 +1,18 @@
+﻿using BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public interface IBrandRepository
+    { 
+        public List<TblBrand> GetAllList();
+        public TblBrand GetBrand(int id);
+        public void Update(TblBrand brand);
+        public void Delete(int id);
+        public void Create(TblBrand brand);
+    }
+}
---
+++ Repositories/IModelRepository.cs
@@ -0,0 +1,18 @@
+﻿using BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories
+{
+    public interface IModelRepository
+    { 
+        public List<TblModel> GetAllList();
+        public TblModel GetModel(int id);
+        public void Update(TblModel Model);
+        public void Delete(int id);
+        public void Create(TblModel Model);
+    }
+}
